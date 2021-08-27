e01706a9495333b640b26ee069c536c3001132b7
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2021-08-27 23:37:38 +0200

    Add documentation

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # InstaSaved
 A simple command line tool to download all images you have saved on Instagram
+
+# Disclaimer
+I used code from [pietrocaselani](https://github.com/pietrocaselani)'s [Instagram Saved Posts](https://github.com/pietrocaselani/Instagram-Saved-Posts) and integrated it into this project. Make sure to check out his repository aswell!
+
+# Installation
+Similar to [TwitterBookmarks](https://github.com/Stridsvagn69420/TwitterBookmarks#framework-dependent-binary), the execution script must be inside a folder registered in your PATH variable (e.g. /usr/bin on Linux and other Unix-like systems):  
+
+Clone the repository, run `npm i` to install all Node modules needed and add a `cd /path/to/local/instasaved/folder` into the `instasave` script file (Node.js and NPM required)
+
+## Adding credentials
+Also similar to [TwitterBookmarks](https://github.com/Stridsvagn69420/TwitterBookmarks#platform-binary), a config file is needed where credentials and other settings are stored in.  
+Create a `config.json` file inside `.config/instasave` located in your home directory that should look like this:
+```js
+{
+    "username": "your_username", //Your username without the @
+    "password": "your_password", //Your password
+    "twoFA_key": "BASE32_TOTP_KEY", //You only have to use this if you use 2FA
+	"downloadDir": "/path/to/instagram/images", //Absolute path to where you want your images saved 
+	"delay": 50 //The delay in milliseconds needed between downloads. Change this value if you're facing errors.
+}
+```
+Note that you should make the file only able to be read by you and no one else since your credentials are stored in there (as well as your 2FA key! 2FA won't help you on a data breach!)
+
+## Adding 2FA key
+This step is only neccesary if you're using 2FA with e.g. Google Authenticator (SMS 2FA is not supported yet).  
+
+If your Authenticator app doesn't support directly exporting the key as text but a QR Code (like Google Authenticator) and you didn't save the key anywhere else/didn't get a key at all, copy the QR Code and use a reader to get the key in text. Google Lens supports directly reading text from QR Codes.
+
+If you're using WinAuth, you can directly copy it by right-clicking on the account and then copying the Secrect Key into the `config.json` file.
---
+++ insta.js
@@ -0,0 +1,171 @@
+//imports
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+const { username, password, twoFA_key, downloadDir, delay } = require(path.join(os.homedir(), ".config/instasave/config.json"));
+const { IgApiClient } = require("instagram-private-api");
+const totp = require("totp-generator");
+const fetch = require("node-fetch");
+const delayMS = ms => new Promise((resolve, reject) => setTimeout(() => resolve(), ms));
+
+//main function
+function main(auth) {
+    console.log(`[${new Date().toLocaleTimeString()}] Successfully logged in as ${auth.full_name} (@${auth.username})!`);
+    fetchPosts(ig.feed.saved(auth.pk))
+    .then(async posts => {
+        var allposts = getURLs(posts);
+        var eta = Math.floor(delay * allposts.length / 60000);
+        console.log(`[${new Date().toLocaleTimeString()}] Starting download of ${allposts.length} images in ${posts.length} posts... (ETA: ${eta}m)`);
+        for (i = 0; i < allposts.length; i++) {
+            var imageURL = allposts[i].url;
+            var filename = allposts[i].post;
+            var filepath = path.join(downloadDir, filename);
+            if (fs.existsSync(filepath)) {
+                console.log(`[${i+1}] ${filename} already exists!`);
+            } else {
+                try {
+                    var result = await fetch(imageURL);
+                    var data = await result.buffer();
+                    fs.writeFileSync(filepath, data);
+                    console.log(`[${i+1}] Downloaded ${filename}!`);
+                    await delayMS(delay);
+                } catch (e) {
+                    console.error(`[${new Date().toLocaleTimeString()}] An error occured on image No.${i+1}!`);
+                    console.error(e);
+                    setTimeout(() => {
+                        fetch(imageURL).then(async x => {
+                            var data = await x.buffer();
+                            fs.writeFileSync(filepath, data);
+                            console.log(`[${i+1}] Downloaded ${filename}!`);
+                            await delayMS(delay);
+                        }).catch(() => console.log(`[${i+1}] Could not download ${filename}...`));
+                    }, delay * 2);
+                }
+            }
+        }
+        console.log(`[${new Date().toLocaleTimeString()}] Finished downloading all images! Exiting...`);
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+}
+
+//create Instagram client and log in
+const ig = new IgApiClient();
+ig.state.generateDevice(username);
+ig.account.login(username, password)
+.then(main)
+.catch(err => {
+    const { username, two_factor_identifier } = err.response.body.two_factor_info;
+    ig.account.twoFactorLogin({
+        username,
+        verificationCode: `${totp(twoFA_key)}`,
+        twoFactorIdentifier: two_factor_identifier,
+        verificationMethod: '0',
+        trustThisDevice: '1'
+    })
+    .then(main)
+    .catch(console.error);
+});
+
+//extra stuff nedded for the main function
+/**
+ * @param {Array<SavedPost>} rawPosts The result from fetchPosts() 
+ * @returns {Array<formattedURLs>} All urls
+*/
+function getURLs(rawPosts) {
+    console.log(`[${new Date().toLocaleTimeString()}] Reading URLS...`);
+    var output = [];
+    for (i = 0; i < rawPosts.length; i++) {
+        for (int = 0; int < rawPosts[i].imagesLink.length; int++) {
+            output.push({
+                url: rawPosts[i].imagesLink[int],
+                post: `${int}_${rawPosts[i].code}.jpg`
+            });
+        }
+    }
+    return output;
+}
+class formattedURLs {
+    url;
+    post;
+}
+/*
+Disclaimer: Following code is entirely copied from https://github.com/pietrocaselani/Instagram-Saved-Posts and only one modification was made:
+BEGIN
+*/
+class SavedPost {
+    constructor(webLink, code, caption, imagesLink) {
+      this.webLink = webLink;
+      this.code = code;
+      this.caption = caption;
+      this.imagesLink = imagesLink;
+    }
+  }
+const urlsFromCandidates = (candidates) => {
+    let maxWidth = 0;
+    let maxHeight = 0;
+    let correctURL = '';
+  
+    candidates.forEach((candidate) => {
+      if (candidate.width > maxWidth && candidate.height > maxHeight) {
+        const { width, height, url } = candidate;
+        maxWidth = width;
+        maxHeight = height;
+        correctURL = url;
+      }
+    });
+  
+    return [correctURL];
+  };
+  
+  const urlsFromCarousel = (carousel_media) => {
+    const urls = carousel_media.map((element) => {
+      return urlsFromCandidates(element.image_versions2.candidates);
+    });
+  
+    return [].concat(...urls);
+  };
+  
+  const urlFromMedia = (media) => {
+    const images = media.image_versions2;
+  
+    if (!images) {
+      return urlsFromCarousel(media.carousel_media);
+    }
+  
+    return urlsFromCandidates(images.candidates);
+  };
+  
+  const postFromMedia = (media) => {
+    const { code } = media;
+    const webLink = `https://www.instagram.com/p/${code}`;
+    const caption = media.caption ? media.caption.text : '';
+    const imagesLink = urlFromMedia(media);
+  
+    return new SavedPost(webLink, code, caption, imagesLink);
+  };
+  
+  
+  const postsFromItems = (items) => {
+    return items.map((item) => {
+      return postFromMedia(item);
+    });
+  };
+  
+  const fetchPostsRecursive = async (savedFeed, posts) => {
+    const items = await savedFeed.items();
+    const allPosts = posts.concat(postsFromItems(items));
+    return savedFeed.isMoreAvailable() ? fetchPostsRecursive(savedFeed, allPosts) : allPosts;
+  };
+  
+  const fetchPosts = async (savedFeed) => {
+    const allPosts = [];
+    console.log(`[${new Date().toLocaleTimeString()}] Fetching posts... (This might take a while)`); //this is the only modification
+    return fetchPostsRecursive(savedFeed, allPosts);
+  };
+/*
+END
+*/
